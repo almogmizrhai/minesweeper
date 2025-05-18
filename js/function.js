01b52ca4a43175b8d847f43c245c502a7d3ee79e
almogmizrhai <almogosh111@gmail.com>
2025-05-18 19:22:26 +0300

    Generate gBoard Matrix
    Step1 – the seed app: 
    1. Create a 4x4 gBoard Matrix containing Objects.
    2. Set 2 of them to be mines.
    3. Present the board using renderBoard() function

--- js/function.js
+++ js/function.js
@@ -1,23 +1,38 @@
 'use strict'
 
 
+function createCell(){
+    var gBoardCell = { 
+        minesAroundCount: 4, 
+        isRevealed: false, 
+        isMine: false, 
+        isMarked: false, 
+    }
+    return gBoardCell
+}
+
 function generateMat() {
 	const mat = []
+
 
 	for (var i = 0; i < gLevel.SIZE; i++) {
 		mat[i] = []
 
 		for (var j = 0; j < gLevel.SIZE; j++) {
-			mat[i][j] = gBoardCell
+			mat[i][j] = createCell()
 		}
 	}
+
+    mat[2][1].isMine = true
+    mat[0][2].isMine = true
+
 	return mat
 }
 
-function onChangeDifficulty(level){
+function onChangeDifficulty(level, mines){
     gLevel.SIZE = level
-    gLevel.MINES = (level/2)
-    // console.log(gLevel)
+    gLevel.MINES = mines
+    console.log('mines on board:', gLevel.MINES)
     onInit()
 }
 
